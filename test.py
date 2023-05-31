d1195265ed0e46695b03c4a0b9b9a77a07ff89bf
lucasmrdt <lucas.marandat@gmail.com>
2023-05-31 17:06:12 +0200

    fix workflow [11]

--- test.py
+++ test.py
@@ -2,7 +2,9 @@ import gymnasium as gym
 from gymnasium.wrappers.record_video import RecordVideo
 import sailboat_gym
 
-env = gym.make('SailboatLSAEnv-v0', renderer=sailboat_gym.CV2DRenderer())
+env = gym.make('SailboatLSAEnv-v0',
+               renderer=sailboat_gym.CV2DRenderer(),
+               container_tag='realtime')
 env = RecordVideo(env, video_folder='./output/videos/')
 
 env.reset(seed=10)
