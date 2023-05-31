019aba043fab4bb36340bab0514c5ee4bfa74758
lucasmrdt <lucas.marandat@gmail.com>
2023-05-31 09:30:11 +0200

    add hook and test scenario

---
+++ test.py
@@ -0,0 +1,17 @@
+import gymnasium as gym
+from gymnasium.wrappers.record_video import RecordVideo
+import sailboat_gym
+
+env = gym.make('SailboatLSAEnv-v0', renderer=sailboat_gym.CV2DRenderer())
+env = RecordVideo(env, video_folder='./output/videos/')
+
+env.reset(seed=10)
+
+while True:
+    act = env.action_space.sample()
+    obs, reward, terminated, truncated, info = env.step(act)
+    if truncated:
+        break
+    env.render()
+
+env.close()
